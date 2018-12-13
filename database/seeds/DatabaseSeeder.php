077743127be09b18162c232380433db692336aca
Nico Purnama Latena <nicopurnamalatena@gmail.com>
2018-12-14 00:55:55 +0700

    ubah database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserTableSeeder::class);
         $this->call(KendaraanTableSeeder::class);
         $this->call(ProfileSeeder::class);
+		// ini tambahan dari ocin
     }
 }
